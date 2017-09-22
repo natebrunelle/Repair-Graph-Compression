09cd258dc516eadff10999af5fd6306b9687d59f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-09-22 16:20:10 -0400

    deleted duplicated file

--- compression.py
+++
@@ -1,9 +0,0 @@
-''' Functions that will handle the compression stuff: 
-
-Function 1: scan through the adjacency list of a node and update the dictionary
-Function 2: get the top pair from the dictionary and replace it with new node everywhere it appears [then call function 1]
-
-'''
-
-# "pair": [count, [[node #, index],[ ... ]]]
-repairDictionary={}
