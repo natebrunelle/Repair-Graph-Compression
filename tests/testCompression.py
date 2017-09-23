2c1ae5658423c163e6cbacb725d4400750f2b8d3
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-09-23 17:39:48 -0400

    added class

--- tests/testCompression.py
+++ tests/testCompression.py
@@ -1,4 +1,22 @@
 import unittest
+from unittest import TestCase
+import networkx as nx
+
+
+class TestScanFunction(TestCase):
+    directedGraph=nx.DiGraph()
+    
+    def setUp(self):
+        #create graph
+        global directedGraph
+        directedGraph.add_edges_from([([2, True],[8, True]), (2,5), (2, 6), (2,9), (3,5), (3, 8), (3, 6), (1, 2), (4, 3)])
+
+    #testing test 
+    def testScan(self):
+        global directedGraph
+        print(list(directedGraph.neighbors(2)))
+        print(list(directedGraph.neighbors(3)))
+        
 
 
 
