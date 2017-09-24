cec609c3a700a2d6e2f7929de12598bf9ce00cb6
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-09-23 23:53:22 -0400

    added a few tests for the dic update and started work on my function

--- repair/compression.py
+++ repair/compression.py
@@ -4,12 +4,11 @@ Function 1: scan through the adjacency list of a node and update the dictionary
 Function 2: get the top pair from the dictionary and replace it with new node everywhere it appears [then call function 1]
 
 '''
-#todo: how should we identify nodes that are there for re-pairing and actual nodes?
 
 # "pair": [count, [[node #, index],[ ... ]]]
-repairDictionary={}
 
 def updateDictionary(adjList):
+    repairDictionary={}
     for node in adjList.keys():
         for j in range(0, len(adjList[node])-1):
             numSet = (adjList[node][j],adjList[node][j+1])
@@ -19,5 +18,11 @@ def updateDictionary(adjList):
                 repairDictionary[numSet][1].append((node, j))
             else:
                 repairDictionary[numSet] = [1, [(node, j)]]
+    
     return repairDictionary
 
+def repair(adjList):
+    #generate the repair dictionary
+    repairDictionary=updateDictionary(adjList)
+
+    
--- tests/testCompression.py
+++ tests/testCompression.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest import TestCase
-from repair.node import Node
 from repair.compression import *
 
 class TestScanFunction(TestCase):
@@ -8,38 +7,84 @@ class TestScanFunction(TestCase):
     
     def setUp(self):
         #create graph
+        node1=(1, False)
         node2=(2, False)
         node3=(3, False)
-        node1=(1, False)
         node4=(4, False)
         node5=(5, False)
         node6=(6, False)
+        node7=(7, False)
         node8=(8, False)
-        
-        self.directedGraph[2]=[node5,
-                                node6,
-                                node8]
+
+
+        self.directedGraph[1]=[node5,node6,node7,node8]        
+        self.directedGraph[2]=[node5,node6,node7,node8]
+        self.directedGraph[3]=[node5,node6,node7,node8]
+        self.directedGraph[4]=[node5,node6,node7,node8]
         self.directedGraph[5]=[]
         self.directedGraph[6]=[]
-        self.directedGraph[4]=[node3]
+        self.directedGraph[7]=[]
         self.directedGraph[8]=[]
-        self.directedGraph[1]=[node5,
-                                node6,
-                                node8]
-        self.directedGraph[3]=[node2]
                                      
 
-    #testing test 
-    def testScan(self):
-        directedGraph=self.directedGraph
+    def testUpdateDicCount(self):
+        pairs=updateDictionary(self.directedGraph)
+
+        expected=4
+        actual=pairs[((5,False),(6,False))][0]
         
+        self.assertEqual(expected,actual)
 
-    def testUpdateDic(self):
-        compDic=updateDictionary(self.directedGraph)
-        for key in compDic.keys():
-            print(str(key)+"\t"+str(compDic[key]))
+    def testUpdateDicCountEmpty(self):
+        directedGraph={}
+        expected=0
 
+        self.assertEqual(expected,len(updateDictionary(directedGraph)))
 
+    def testUpdateDicCountNoCommon(self):
+        node5=(5, False)
+        node6=(6, False)
+        node7=(7, False)
+        node8=(8, False)
+
+        self.directedGraph[1]=[node5,node6]        
+        self.directedGraph[2]=[node6,node7,node8]
+        self.directedGraph[3]=[node5,node7,node8]
+        self.directedGraph[4]=[node8]
+
+        expected=1
+
+        pairs=updateDictionary(self.directedGraph)
+        actual=pairs[((5,False),(6,False))][0]
+        
+        self.assertEqual(expected, actual)
+
+    def testUpdateOrderAffectsCount(self):
+        node5=(5, False)
+        node6=(6, False)
+        node7=(7, False)
+        node8=(8, False)
+
+        self.directedGraph[1]=[node6,node5,node8,node7]        
+        self.directedGraph[2]=[node7,node6,node8,node5]
+        self.directedGraph[3]=[node8,node6,node7,node5]
+        self.directedGraph[4]=[node5,node6,node7,node8]
+
+        expected=1
+
+        pairs=updateDictionary(self.directedGraph)
+        actual=pairs[((5,False),(6,False))][0]
+        
+        self.assertEqual(expected, actual)
+
+        actual2=pairs[((6,False),(5,False))][0]
+        self.assertEqual(expected, actual)
+        
+    
+    def testRepairWithTwo(self):
+        repaired=repair(self.directedGraph)
+        print(repaired)
+        
 
 if __name__=='__main__':
     unittest.main()
