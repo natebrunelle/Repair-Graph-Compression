92c178b0591c8caa79b8ee4e369b58ce651623d7
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2017-12-01 19:01:32 -0500

    working version

--- repair/graphGenerator.py
+++ repair/graphGenerator.py
@@ -6,7 +6,7 @@ def weaklyConnectedClusters(clusterSize, clusterNum, edgeNum):
     ids=list(string.ascii_lowercase) #todo this limits us to 26 characters fix with A1, A1...
 
     #for testing only
-    random.seed(5)
+#    random.seed(15)
     
     #get all the individual clusters
     for c in range(clusterNum):
@@ -21,31 +21,35 @@ def weaklyConnectedClusters(clusterSize, clusterNum, edgeNum):
 
     for i in range(clusterNum):
         for e in range(edgeNum):
-            #choose two random clusters
-            cluster1=random.randint(0, len(clusters.keys())-1)
-            cluster2=random.randint(0, len(clusters.keys())-1)
+            #choose two random nodes
+            cluster1=i*clusterSize
+            cluster2=random.randint(1, clusterNum+1)
 
-            while(cluster1==cluster2):
-                cluster1=random.randint(0, len(clusters.keys())-1)
-                cluster2=random.randint(0, len(clusters.keys())-1)
-                
-
-            print("Cluster 1: "+str(cluster1)+"\t Cluster 2: "+str(cluster2))
             #get clusters
             cluster1=list(clusters.keys())[cluster1][1]
-            cluster2=list(clusters.keys())[cluster2][1]
+            cluster2=list(clusters.keys())[clusterNum*cluster2][1]
 
+            while(cluster1==cluster2):
+                cluster2=random.randint(1, clusterNum+1)
+                cluster2=list(clusters.keys())[clusterNum*cluster2][1]
+                
+            print("Cluster 1: "+str(cluster1)+"\t Cluster 2: "+str(cluster2))
 
+            
             #randomly pick nodes from the clusters
             node1=random.randint(1, clusterSize-1)
             node2=random.randint(1, clusterSize-1)
 
-
+            
             node1=(node1, cluster1)
             node2=(node2, cluster2)
             
-            clusters[node1].append(node2)
+            while(node2 in clusters[node1]):
+                node2=random.randint(1, clusterSize-1)
+                node2=(node2, cluster2)
 
+            clusters[node1].append(node2)
+            print("----------------")
 
     for node in clusters.keys():
         for t in clusters[node]:
