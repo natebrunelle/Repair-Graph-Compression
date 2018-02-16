37b99510ec05701e9b640e3aba37a44a98dc78fb
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-02-16 16:22:40 -0500

    created failing tests

---
+++ tests/test_compression.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest import TestCase
+
+from repair.compression import *
+
+
+class TestCompressionDictionary(TestCase):
+    def setUp(self):
+        pass
+
+    def test_is_empty(self):
+        ''' simple test for empty '''
+        self.fail("No test")
+
+    def test_most_common(self):
+        ''' tests most common with no ties '''
+        self.fail("No test")
+
+    def test_most_common_duplicates(self):
+        ''' tests most common when there are ties'''
+        self.fail("No test")
+
+    def test_most_common_empty(self):
+        ''' tests most common when there no nodes '''
+        self.fail("No test")
+
+    def test_update_dictionary_empty_graph(self):
+        ''' tests update dic with an empty graph '''
+        self.fail("No test")
+
+    def test_update_dictionary_pair_occurs_once(self):
+        ''' tests update dictionary where the pair shows up only once in the graph '''
+        self.fail("No test")
+
+    def test_update_dictionary_pair_occurs_multiple(self):
+        ''' tests update where a pair shows up multiple times in the graph '''
+        self.fail("No test")
