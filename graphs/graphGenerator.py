7d8481fa2ca952ff7bf9a6a8fa38c9a3b432a63d
jinniespark <jinniespark@gmail.com>
2018-03-05 23:14:56 -0500

    working version

--- graphs/graphGenerator.py
+++ graphs/graphGenerator.py
@@ -1,67 +1,116 @@
 import random
 import string
+from graphs.graph import Graph
+from nodeAndRepairNode.nodes import Node
 
 
-def weaklyConnectedClusters(number_of_connections, clusterSize, clusterNum,
-                            edgeNum):
-    clusters = {}
-    bins = clusterNum % 26
 
-    ids = []
-    for char in list(string.ascii_lowercase):
-        for i in range(bins):
-            ids.append(char + str(i))
+#We are particularly interested in: 
+#number of clusters (groups of graphs), 
+#the density of connection within these clusters, 
+#the density of edges between the clusters,
+#some form of randomization (how the connections happen etc).
 
-            if len(ids) == clusterNum:
-                break
-        if len(ids) == clusterNum:
-            break
+def weaklyConnectedClusters(number_of_connections, graphSize, graphNum,
+                            edgeNum, graphName):
+    
+    clusters = [ [] for a in range(graphNum) ]
 
-    #for testing only
-    #random.seed(15)
+    
+    #clusters = {}
 
-    #get all the individual clusters
-    for c in range(clusterNum):
-        id = ids[c]
-        hub = createHub(number_of_connections, clusterSize, id)
-        for node in hub.keys():
-            clusters[node] = hub[node]
+    for i in range(graphNum):
+        for j in range(graphSize):
+            self.n = Node(random.choice(string.letters))
+            clusters[i].append(n)
 
-    for i in range(clusterNum):
-        for e in range(edgeNum):
-            #choose two random nodes
-            cluster1 = i * clusterSize
-            cluster2 = random.randint(1, clusterNum - 1)
+    graphs = []
+    for nodeList in clusters:
+        self.g = graphName(clusters[nodeList][0], 1)
+        for i in range(edgeNum):
+            g.add_edge(random.choice(g.list_nodes), random.choice(nodeList))
+        graphs.append(g)
 
-            #get clusters
-            cluster1 = list(clusters.keys())[cluster1][1]
-            cluster2 = list(clusters.keys())[cluster2 * clusterSize][1]
+    counter = 0
+    connected = [ false for i in range(graphNum)]
+    while(counter < graphNum):
+        random1 = random.randint(0, graphNum-1)
+        random2 = random.randint(0, graphNum-1)
+        if(random1 != random2 && (connected[random1] == false || connected[random2] == false)):
+            graphs[random1].add_edge(random.choice(graphs[random1].list_nodes), random.choice(graphs[random2].list_nodes))
+            counter++
+        
+    while(counter < number_of_connections):
+        random1 = random.randint(0, graphNum-1)
+        random2 = random.randint(0, graphNum-1)
+        #NEED TO CHECK IF THEY ARE ALREADY CONNECTED --> if they are connected then repeat until you find a pair that hasn't been connected && connect them
+        graphs[random1].add_edge(random.choice(graphs[random1].list_nodes), random.choice(graphs[random2].list_nodes))
+        counter ++
+        
 
-            while (cluster1 == cluster2):
-                cluster2 = random.randint(1, clusterNum - 1)
-                cluster2 = list(clusters.keys())[cluster2 * clusterSize][1]
 
-            #randomly pick nodes from the clusters
-            node1 = random.randint(1, clusterSize - 1)
-            node2 = random.randint(1, clusterSize - 1)
 
-            node1 = (node1, cluster1)
-            node2 = (node2, cluster2)
 
-            while (node2 in clusters[node1]):
-                node2 = random.randint(1, clusterSize - 1)
-                node2 = (node2, cluster2)
 
-            clusters[node1].append(node2)
+    
+    # clusters = {}
+    # bins = clusterNum % 26
 
-    for node in clusters.keys():
-        for t in clusters[node]:
-            if t[0] == -1:
-                n = clusters[node]
-                del n[0]
-                clusters[node] = n
+    # ids = []
+    # for char in list(string.ascii_lowercase):
+    #     for i in range(bins):
+    #         ids.append(char + str(i))
 
-    return clusters
+    #         if len(ids) == clusterNum:
+    #             break
+    #     if len(ids) == clusterNum:
+    #         break
+
+    # #for testing only
+    # #random.seed(15)
+
+    # #get all the individual clusters
+    # for c in range(clusterNum):
+    #     id = ids[c]
+    #     hub = createHub(number_of_connections, clusterSize, id)
+    #     for node in hub.keys():
+    #         clusters[node] = hub[node]
+
+    # for i in range(clusterNum):
+    #     for e in range(edgeNum):
+    #         #choose two random nodes
+    #         cluster1 = i * clusterSize
+    #         cluster2 = random.randint(1, clusterNum - 1)
+
+    #         #get clusters
+    #         cluster1 = list(clusters.keys())[cluster1][1]
+    #         cluster2 = list(clusters.keys())[cluster2 * clusterSize][1]
+
+    #         while (cluster1 == cluster2):
+    #             cluster2 = random.randint(1, clusterNum - 1)
+    #             cluster2 = list(clusters.keys())[cluster2 * clusterSize][1]
+
+    #         #randomly pick nodes from the clusters
+    #         node1 = random.randint(1, clusterSize - 1)
+    #         node2 = random.randint(1, clusterSize - 1)
+
+    #         node1 = (node1, cluster1)
+    #         node2 = (node2, cluster2)
+
+    #         while (node2 in clusters[node1]):
+    #             node2 = random.randint(1, clusterSize - 1)
+    #             node2 = (node2, cluster2)
+
+    #         clusters[node1].append(node2)
+
+    # for node in clusters.keys():
+    #     for t in clusters[node]:
+    #         if t[0] == -1:
+    #             n = clusters[node]
+    #             del n[0]
+    #             clusters[node] = n
+
+    # return clusters
 
 
 def createHub(number_of_connections, size, clusterID):
