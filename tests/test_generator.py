1da35a8ad688f9f0b99ad4dba4870cf2dcc2f2f1
Rahul Tuladhar <rt4hc@virginia.edu>
2018-03-26 12:58:16 -0400

    test_generator start

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,28 +3,53 @@ from graphs.graph_factory import GraphTypes
 from graphs.graph_factory import GraphFactory
 from graphs.graph_factory import GraphFactoryNoData
 from graphs.graph_factory import GraphFactoryAlphaNumeric
-from graphs.graph_generator import weakly_connected_graph
+from graphs.graph_generator import weakly_connected_graphs
 from nodeAndRepairNode.nodes import Node
+from unittest import TestCase
 from graphs.graph import Graph
 
 
-class GraphGeneratorTestCase(unittest.TestCase):
+class TestGraphGenerator(TestCase):
     def setUp(self):
         self.n1 = Node(1)
         self.n2 = Node(2)
         self.n3 = Node(3)
         self.n4 = Node(4)
         # self.g.list_nodes = [Node(1), Node(2), Node(3)]
-        self.g = Graph([self.n1, self.n2, self.n3])
-        self.gf = GraphFactory(1, 10)
+        # self.g = Graph([self.n1, self.n2, self.n3])
+        self.gf = GraphFactoryAlphaNumeric(GraphTypes(3), 10)
+        #weakly_connected_graphs(connection_num, graph_num, edge_num, graph_factory):
+        self.g = weakly_connected_graphs(10, 20, 11, self.gf)
 
-    def test_graph_generator(self):
-        g = weakly_connected_graph(10, 20, 10, self.gf);
-        self.assertEqual(gf., )
+    # def test_graph_generator(self):
+    #     # weakly_connected_graphs(connection_num, graph_num, edge_num, graph_factory):
+    #     g = weakly_connected_graph(10, 20, 10, self.gf)
+    #     self.assertEqual(self.gf )
 
-        #connections
-        #number of nodes
-        #number of graphs
-        #if connections are valid
-        #
+    # connections
+    def test_num_connections(self):
+        self.assertEqual(len(self.g.list_nodes), 10, "Wrong number of connections between graphs")
+
+
+    # number of nodes
+    def test_number_of_nodes(self):
+        all_true = True
+        for graph in self.g:
+            if len(graph.list_nodes is not 100):
+                all_true = False
+        self.assertEqual(all_true, True, "The number of nodes in the graphs are not correct")
+
+    # number of graphs
+    def test_number_of_graphs(self):
+        self.assertEqual(len(self.g), 20, "The number of graphs is not correct ")
+
+
+    # # number of graphs
+    # def test_edge_num_of_graphs(self):
+    #     pass
+    #
+    # # if connections are valid1
+    # def test_correct_connections(self):
+    #     pass
+
 
