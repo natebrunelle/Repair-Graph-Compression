54146885601fdb59739cf86771aef949c8f4f7d3
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-07-02 23:11:38 -0700

    Tested the happy path for observable feature. There some issues with integration with repair that I haven't tested yet.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,7 +1,7 @@
 import unittest
 
-from graphs.graph import *
-from nodeAndRepairNode.nodes import Node
+from graphs.graph import Graph
+from nodeAndRepairNode.nodes import EventType, Node, RepairNode
 
 
 class GraphTestCase(unittest.TestCase):
@@ -34,37 +34,84 @@ class GraphTestCase(unittest.TestCase):
         self.n5.edges = []
         self.n6.edges = []
 
+    def test_update_delete(self):
+        self.h.add_edge(self.n5, self.n1)
+        self.assertIn(self.n1, self.n5.edges)
+
+        self.g.delete_node(self.n1)
+        self.assertNotIn(self.n1, self.g.list_nodes)
+        self.assertNotIn(self.n1, self.n5.edges)
+
+    def test_update_replace(self):
+        replacement = RepairNode(-1, self.n5, self.n6)
+        self.g = Graph([self.n1])
+        self.h = Graph([self.n4])
+
+        self.g.add_edge(self.n1, self.n5)
+        self.g.add_edge(self.n1, self.n6)
+
+        self.h.add_edge(self.n4, self.n5)
+        self.h.add_edge(self.n4, self.n6)
+
+        self.n5.notify_all(EventType.node_replaced, [self.n6, replacement])
+
+        self.assertNotIn(self.n5, self.n1.edges,
+                         "Replaced nodes shouldn't exit")
+        self.assertNotIn(self.n5, self.n4.edges,
+                         "Replaced nodes shouldn't exit")
+
+        self.assertNotIn(self.n6, self.n1.edges,
+                         "Replaced nodes shouldn't exit")
+        self.assertNotIn(self.n6, self.n4.edges,
+                         "Replaced nodes shouldn't exit")
+
+        self.assertIn(replacement, self.n1.edges,
+                      "Replacement node should exit")
+        self.assertIn(replacement, self.n4.edges,
+                      "Replacement node should exit")
+
     def test_add_edge(self):
         self.g.add_edge(self.n1, self.n2)
         self.assertIn(self.n2, self.n1.edges, "Adj list: edge not added")
-        self.assertEqual(self.n1.edges.count(self.n2), 1, "Adj list: n2 should appear 1x, no duplicates")
-        self.assertCountEqual(self.n1.edges, [self.n2], "Adj list not exactly as expected")
+        self.assertEqual(
+            self.n1.edges.count(self.n2), 1,
+            "Adj list: n2 should appear 1x, no duplicates")
+        self.assertCountEqual(self.n1.edges, [self.n2],
+                              "Adj list not exactly as expected")
 
     def test_add_outside_edge(self):
         self.assertNotIn(self.n4, self.g.list_nodes)  # n4 is outside graph
         self.g.add_edge(self.n1, self.n4)
         self.assertIn(self.n4, self.n1.edges, "Adj lists not updated")
-        self.assertCountEqual(self.n1.edges, [self.n4], "Adj list not exactly as expected")
+        self.assertCountEqual(self.n1.edges, [self.n4],
+                              "Adj list not exactly as expected")
 
     def test_duplicate_add_edge(self):
         # node count shouldn't change
         self.g.add_edge(self.n1, self.n2)  # n2 is added to n1's list
         self.g.add_edge(self.n1, self.n2)
-        self.assertNotIn(self.n1, self.n2.edges)  # n1 is not added to n2's list
+        self.assertNotIn(self.n1,
+                         self.n2.edges)  # n1 is not added to n2's list
         self.assertEqual(self.n1.edges.count(self.n2), 1)
-        self.assertCountEqual(self.n1.edges, [self.n2], "Adj list not exactly as expected")
+        self.assertCountEqual(self.n1.edges, [self.n2],
+                              "Adj list not exactly as expected")
 
     def test_edge_to_self(self):
         self.g.add_edge(self.n1, self.n1)
         self.assertNotIn(self.n1, self.n1.edges)
-        self.assertCountEqual(self.n1.edges, [], "Adj list not exactly as expected")
+        self.assertCountEqual(self.n1.edges, [],
+                              "Adj list not exactly as expected")
 
-    def test_add_node(self):  # should add to list_nodes, this is diff between add_node and add_edge
+    def test_add_node(
+            self
+    ):  # should add to list_nodes, this is diff between add_node and add_edge
         self.assertNotIn(self.n4, self.g.list_nodes, "n4 is already in graph")
         self.g.add_node(self.n4)
         self.assertIn(self.n4, self.g.list_nodes, "list_nodes not updated")
-        self.assertEqual(self.g.graph_id, self.n4.graph_id, "UUID not set to graph_id")
-        self.assertCountEqual(self.g.list_nodes, self.testList4, "Adj list not exactly as expected")
+        self.assertEqual(self.g.graph_id, self.n4.graph_id,
+                         "UUID not set to graph_id")
+        self.assertCountEqual(self.g.list_nodes, self.testList4,
+                              "Adj list not exactly as expected")
 
     def test_graph_duplicate_add_node(self):
         self.assertIn(self.n3, self.g.list_nodes)
@@ -91,46 +138,64 @@ class GraphTestCase(unittest.TestCase):
         self.assertCountEqual(self.g.list_nodes, self.testList4)
 
     def test_delete_edge(self):
-        self.assertTrue(self.n1 in self.g.list_nodes or self.n2 in self.g.list_nodes, "both parameters not in graph")
+        self.assertTrue(
+            self.n1 in self.g.list_nodes or self.n2 in self.g.list_nodes,
+            "both parameters not in graph")
         self.g.add_edge(self.n1, self.n2)
         self.g.delete_edge(self.n1, self.n2)
 
-        self.assertEqual(len(self.g.list_nodes), 3, "Node_count for graph incorrect")
-        self.assertEqual(self.g.list_nodes, self.testList3, "Graph modified by delete_edge")
+        self.assertEqual(
+            len(self.g.list_nodes), 3, "Node_count for graph incorrect")
+        self.assertEqual(self.g.list_nodes, self.testList3,
+                         "Graph modified by delete_edge")
         # an edge, but not a node deleted, n2 still in list_nodes and the graph
-        self.assertNotIn(self.n2, self.n1.edges, "All instances not removed from adj list, edge(s) not deleted")
+        self.assertNotIn(
+            self.n2, self.n1.edges,
+            "All instances not removed from adj list, edge(s) not deleted")
 
     # TODO: add tests for where second param for delete_edge() outside graph
 
     def test_delete_outside_edge(self):
-        self.assertNotIn(self.n4, self.g.list_nodes, "n4 is already in graph, TESTS ARE STUPID")
+        self.assertNotIn(self.n4, self.g.list_nodes,
+                         "n4 is already in graph, TESTS ARE STUPID")
         self.assertNotIn(self.n5, self.g.list_nodes)
-        self.assertFalse(self.n5 in self.g.list_nodes or self.n4 in self.g.list_nodes)
+        self.assertFalse(self.n5 in self.g.list_nodes
+                         or self.n4 in self.g.list_nodes)
         self.h.add_edge(self.n4, self.n5)
         with self.assertRaises(ValueError):
             self.g.delete_edge(self.n4, self.n5)
 
     def test_delete_node(self):
-        self.assertIn(self.n1, self.g.list_nodes, "n1 is not part of this graph to be removed")
-        self.assertNotIn(self.n2, self.n1.edges, "n2 is already in n1.edges")  # since this is true...
-        self.g.add_edge(self.n1, self.n2)  # THIS NEEDS TO BE FULLY TESTED ELSEWHERE
-        self.g.delete_node(self.n1) # ...this is already true
-        self.assertNotIn(self.n1, self.g.list_nodes, "Node not deleted")  # n1 no longer listed in graph
+        self.assertIn(self.n1, self.g.list_nodes,
+                      "n1 is not part of this graph to be removed")
+        self.assertNotIn(self.n2, self.n1.edges,
+                         "n2 is already in n1.edges")  # since this is true...
+        self.g.add_edge(self.n1,
+                        self.n2)  # THIS NEEDS TO BE FULLY TESTED ELSEWHERE
+        self.g.delete_node(self.n1)  # ...this is already true
+        self.assertNotIn(self.n1, self.g.list_nodes,
+                         "Node not deleted")  # n1 no longer listed in graph
         self.assertIn(self.n2, self.g.list_nodes, "Wrong node deleted")
-        self.assertNotIn(self.n1, self.n2.edges, "Found outside reference to deleted node")
-        self.assertEqual(self.n1.edges, [], "Node's adj list not cleared")  # n1 has no adj nodes
+        self.assertNotIn(self.n1, self.n2.edges,
+                         "Found outside reference to deleted node")
+        self.assertEqual(self.n1.edges, [],
+                         "Node's adj list not cleared")  # n1 has no adj nodes
 
     def delete_nonexistent_edge(self):
         # test_delete_nonexistent_edge(self):
-        self.assertNotIn(self.n4, self.g.list_nodes)  # can delete edges btwn graphs
-        self.assertIn(self.n1, self.g.list_nodes)  # one node must be in the graph
+        self.assertNotIn(self.n4,
+                         self.g.list_nodes)  # can delete edges btwn graphs
+        self.assertIn(self.n1,
+                      self.g.list_nodes)  # one node must be in the graph
         self.assertNotIn(self.n4, self.n1.edges)
-        self.g.delete_edge(self.n1, self.n4)  # n4 attempted removed from n1's list
+        self.g.delete_edge(self.n1,
+                           self.n4)  # n4 attempted removed from n1's list
         self.assertNotIn(self.n4, self.n1.edges)
         with self.assertRaises(ValueError):
             # self.n1.edges.remove(self.n4) # test passes with this, should be equivalent to line below
             # ValueError somehow suppressed and not passed up through classes?
-            self.g.delete_edge(self.n1, self.n4)  # n4 attempted removed from n1's list
+            self.g.delete_edge(self.n1,
+                               self.n4)  # n4 attempted removed from n1's list
 
     def test_delete_nonexistent_node(self):
         self.assertNotIn(self.n4, self.g.list_nodes)
@@ -138,7 +203,8 @@ class GraphTestCase(unittest.TestCase):
             self.g.delete_node(self.n4)
 
     def test_delete_outside_node(self):
-        self.assertNotIn(self.n4, self.h.list_nodes, "n4 is already in graph, TESTS ARE STUPID")
+        self.assertNotIn(self.n4, self.h.list_nodes,
+                         "n4 is already in graph, TESTS ARE STUPID")
         self.h.add_edge(self.n5, self.n4)  # 1st param must be in graph
         self.assertNotIn(self.n4, self.h.list_nodes)
         with self.assertRaises(ValueError):
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,6 +1,20 @@
 import unittest
-from uuid import UUID, uuid4
-from nodeAndRepairNode.nodes import Node, RepairNode
+import warnings
+
+from graphs.graph import Graph
+from nodeAndRepairNode.nodes import EventType, Node, RepairNode
+
+
+class TestObserver:
+    def __init__(self):
+        self.called = False
+        self.payload_received = False
+
+    def update(self, event):
+        self.called = True
+        if event.payload:
+            self.payload_received = True
+
 
 class TestNodeAndRepairNode(unittest.TestCase):
     def setUp(self):
@@ -11,6 +25,28 @@ class TestNodeAndRepairNode(unittest.TestCase):
         self.n5 = Node(4)
         self.rnn = RepairNode(11, self.n1, self.n2)
         self.rrn = RepairNode(12, self.rnn, self.n3)
+
+    def test_observer_registration(self):
+        graph = Graph()
+        self.n1.observe(graph)
+
+        self.assertIn(graph, self.n1.observers, "Observer not registered")
+
+    def test_observer_notify_no_payload(self):
+        observer = TestObserver()
+        self.n1.observe(observer)
+        self.n1.notify_all(EventType.node_deleted)
+
+        self.assertTrue(observer.called, "Observer wasn't notified")
+
+    def test_observer_notify_payload(self):
+        observer = TestObserver()
+        self.n1.observe(observer)
+        self.n1.notify_all(EventType.node_replaced, [self.n1, self.n2])
+
+        self.assertTrue(observer.called, "Observer should be  notified")
+        self.assertTrue(observer.payload_received,
+                        "Data should be delievered to observer")
 
     def test_add_edge(self):
         node1 = Node(5, [])
@@ -51,19 +87,25 @@ class TestNodeAndRepairNode(unittest.TestCase):
         node1 = Node(5, [])
         node1.add_edge(self.n2)
         node1.delete_edge(self.n3)  # should fail
-        self.assertEqual(len(node1.edges), 1,
-                         "deleted nonexistent or wrong edge")
-        self.assertIn(self.n2, node1.edges, "deleted nonexistent or wrong edge")
+        self.assertEqual(
+            len(node1.edges), 1, "deleted nonexistent or wrong edge")
+        self.assertIn(self.n2, node1.edges,
+                      "deleted nonexistent or wrong edge")
 
     def test_replace_only_pair(self):
-        r_node = self.n3    # r_node = repair node
-        r_node.add_edge(self.n1)
-        r_node.add_edge(self.n2)
-        r_node.replace(self.n1, self.n2, self.rnn)
-        self.assertIn(
-            self.rnn, r_node.edges, "There is not only 1 node or repair_node not in edges")
-        self.assertEqual(
-            len(r_node.edges), 1, "There is not 1 edge in the AL")
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            r_node = self.n3  # r_node = repair node
+            r_node.add_edge(self.n1)
+            r_node.add_edge(self.n2)
+            r_node.replace(self.n1, self.n2, self.rnn)
+            self.assertIn(
+                self.rnn, r_node.edges,
+                "There is not only 1 node or repair_node not in edges")
+            self.assertEqual(
+                len(r_node.edges), 1, "There is not 1 edge in the AL")
 
     def test_equal_op(self):
         """tests the equal operator override"""
@@ -88,8 +130,8 @@ class TestNodeAndRepairNode(unittest.TestCase):
         self.n4.graph_id = 1
         self.n5.graph_id = 2
 
-        self.n4.graph_id=-1
-        self.n5.graph_id=1
+        self.n4.graph_id = -1
+        self.n5.graph_id = 1
 
         self.assertTrue(self.n5 > self.n4)
         self.assertFalse(self.n4 > self.n5)
@@ -129,7 +171,10 @@ class TestNodeAndRepairNode(unittest.TestCase):
     def test_node_str(self):
         """tests that the node has the right string output"""
         node1 = Node(5, [])
-        self.assertEqual(str(node1), 'ID: ' + str(node1.uid) + '\tValue: [5]', 'Wrong string outputed')
+        self.assertEqual(
+            str(node1), 'ID: ' + str(node1.uid) + '\tValue: [5]',
+            'Wrong string outputed')
+
 
 # def test_add_repair_node_edge():
 # 	self.rnn.
@@ -145,7 +190,6 @@ class TestNodeAndRepairNode(unittest.TestCase):
 # 	n4.delete_edge(r2)
 # 	print(len(n3.edges))
 # 	assert len(n3.edges) == 0, 'Repair edge not deleted'
-
 
 if __name__ == '__main__':
     unittest.main()
