cdf39ec23e987f32564815d7d19b513362feaed5
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-08-05 00:03:27 -0700

    Working Implementation of generator  (#43)
    * rewriting the generator. Tests are infinte looping.
    
    * fixed the infinite loop issue the tests picked up on. The while loop was marking graphs with true without incrementing the counter twice. Fixed by adjusting the iterator control logic.
    
    * Refactored the solution to clean up some of the logic and make it more clear. Extracted the statements inside the while loop into a function and split the conditions into two conditions.
    
    * Basic implementation of the graph generator
    
    This issue has been around for far too long and I finally got around to it today. This provides a basic version of the generator with minimal testing. It doesn't inf loop and it doesn't break randomly so it's a good step forward.
    
    The implementation is very similar to the last two iterations the main difference is that I have cleaned up the code and split it into many functions. This should make debugging it easier whenever it breaks.
    
    We can close Issue #18 now since we have the basics implemented. Track bugs in d/f issues as they come up.

--- graphs/graph_generator.py
+++ graphs/graph_generator.py
@@ -6,77 +6,114 @@ The graphs will be connected as specified by the parameters of the
 function.
 '''
 import random
-import string
 
-from graphs.graph import Graph
-from nodes.nodes import Node
+
+def pick_two_different_items(all_items):
+    '''
+    Picks two different items from a list of items
+
+    :param all_items: a list of all items to pick from
+    '''
+
+    while True:
+        item1 = random.choice(all_items)
+        item2 = random.choice(all_items)
+
+        if item1 != item2:
+            return item1, item2
+
+
+def randomly_create_edges(graphs, edge_num):
+    '''
+    Randomly creates the specified number of edges in the graph
+    without duplicating connections.
+
+    ..warning: It will infinite loop if you  pass a graph
+    with fewer nodes that edge_num.
+    '''
+
+    for graph in graphs:
+        current_edge_num = 0
+        while current_edge_num < edge_num:
+            node1, node2 = pick_two_different_items(graph.list_nodes)
+
+            if node1 not in node2.edges:
+                node2.add_edge(node1)
+                current_edge_num += 1
+            elif node2 not in node1.edges:
+                node1.add_edge(node2)
+                current_edge_num += 1
+
+    return graphs
+
+
+def pick_nodes_connect_incr(graph1, graph2, counter):
+    '''
+    Extracts the edge making logic between two graphs.
+    Picks a node from graph1 and another from graph2.
+    .. note:: The edge goes from graph1 to graph1
+    '''
+    node1 = random.choice(graph1[1].list_nodes)
+    node2 = random.choice(graph2[1].list_nodes)
+
+    node1.add_edge(node2)
+
+    graph1[0] = True
+    counter += 1
+
+    return graph1, graph2, counter
 
 
 def weakly_connected_graphs(connection_num, graph_num, edge_num,
                             graph_factory):
     '''
-    generates a group of connected graphs as specified by the parameters:
+    Generates a group of connected graphs as specified by the parameters:
 
     :param connection_num: the number of connections we expect to see on ave
     :param graph_num: the number of graph in the cluster
-    :param edge_num: the number of edges we expect to see within each graph on ave
-    :param graph_factory: an instance of a graph factory to specify the kind of graphs
+    :param edge_num: the number of edges expected within each graph
+    :param graph_factory: an instance of a graph factory to
 
     :return: A group of 1 or more graphs connected into one large graph
     :return_type: Graph
     '''
 
-    # if bad input, silently correct it
-    # need a min of graph_num connections to ensure everything is connected
+    # validate the params
     if connection_num < graph_num:
-        connection_num = graph_num
+        raise ValueError("The number of connections must at least equal\
+            the number of graphs.")
 
-    # get the graph size
     graph_size = graph_factory.num_of_nodes
 
-    # fix bad input if needed
     if edge_num < graph_size:
-        edge_num = graph_size
-
-    # place holder for the graphs we will create
-    # will help ensure every graph is connected at least once
-    graphs = []
+        raise ValueError("The number of edges must be at least\
+                equal to number of nodes per graph")
 
     # create all the graphs
+    graphs = []
     for _ in range(graph_num):
         graphs.append(graph_factory.get_graph())
 
-    node1 = random.choice(graphs[1].list_nodes)
-    node2 = random.choice(graphs[0].list_nodes)
+    # we start out with 0 connections/every graph not connected
+    connections_so_far = 0
+    connected = [[False, graph] for graph in graphs]
 
-    graphs[0].add_edge(node2, node1)
+    # make sure at least every graph is connected once
+    while connections_so_far < len(graphs):
+        graph1, graph2 = pick_two_different_items(connected)
 
-    #    for i in range(0,len(graphs[0].list_nodes)):
-    #        print(str(graphs[0].list_nodes[i]))
+        if graph1[0] is False:
+            graph1, graph2, connections_so_far = pick_nodes_connect_incr(
+                graph1, graph2, connections_so_far)
 
-    #while counter < graph_num:
-    counter = 0
-    #random2 = random.randint(0, graph_num - 1)
-    #    for i in range(0,len(graphs)):
-    #        #if (random1 != random2 and ((connected[random1] is False or connected[random2] is False))):
-    #        random1 = random.randint(0, graph_num - 1)
-    #        print(random1)
-    #        print("connecting")
-    #        graphs[i].add_edge(random.choice(graphs[i].list_nodes),random.choice(graphs[random1].list_nodes))
-    #        counter += 1
-    #
-    #
-    #    while counter < connection_num:
-    #        print("second loop")
-    #        random1 = random.randint(0, graph_num - 1)
-    #        random2 = random.randint(0, graph_num - 1)
-    #
-    #        randomNode1 = random.choice(graphs[random1].list_nodes)
-    #        randomNode2 = random.choice(graphs[random2].list_nodes)
-    #
-    #        if (randomNode2 in graphs[random1].list_nodes or randomNode1 in graphs[random2].list_nodes):
-    #            continue
-    #        else:
-    #            graphs[random1].add_edge(randomNode1, randomNode2)
-    #            counter = counter + 1
+        if graph2[0] is False:
+            graph1, graph2, connections_so_far = pick_nodes_connect_incr(
+                graph2, graph1, connections_so_far)
+
+    # now we can use the rest of the connections randomly
+    while connections_so_far < connection_num:
+        graph1, graph2 = pick_two_different_items(connected)
+        graph1, graph2, connections_so_far = pick_nodes_connect_incr(
+            graph1, graph2, connections_so_far)
+
     return graphs
---
+++ tests/test_generator.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest import TestCase
+
+from graphs.graph import Graph
+from graphs.graph_factory import (GraphFactory, GraphFactoryAlphaNumeric,
+                                  GraphFactoryNoData, GraphTypes)
+from graphs.graph_generator import (pick_two_different_items,
+                                    randomly_create_edges,
+                                    weakly_connected_graphs)
+from nodes.nodes import Node
+
+
+'''
+    :param connection_num: the number of connections
+    :param graph_num: the number of graph in the cluster
+    :param edge_num: the number of edges expected within each graph
+    :param graph_factory: an instance of a graph factory to
+'''
+
+
+class TestGraphGenerator(TestCase):
+    def setUp(self):
+        self.graph_factory = GraphFactoryAlphaNumeric(GraphTypes.complete, 5)
+
+    def test_all_graphs_connected_atleast_once(self):
+        num_conns = 15
+        num_graphs = 5
+        num_internal_edges = 5
+
+        graphs = weakly_connected_graphs(
+            num_conns, num_graphs, num_internal_edges, self.graph_factory)
+
+        for graph in graphs:
+            for node in graph.list_nodes:
+                for edge in node.edges:
+                    if edge.graph_id != node.graph_id:
+                        # TODO this is probalby not the best way to test this
+                        self.assertNotEqual(edge.graph_id, node.graph_id)
+
+    def test_randomly_create_edges(self):
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(5)
+        n6 = Node(6)
+
+        graph1 = Graph([n1, n2, n3])
+        graph2 = Graph([n4, n5, n6])
+
+        graphs = randomly_create_edges([graph1, graph2], 3)
+
+        for graph in graphs:
+            edge_count = 0
+            for node in graph.list_nodes:
+                edge_count += len(node.edges)
+
+            self.assertEqual(edge_count, 3,
+                             "Every graph should have at least 3 edges.")
+
+    def test_pick_two_randoms(self):
+        population = [1, 2, 3, 4, 5]
+
+        for _ in range(5):
+            item1, item2 = pick_two_different_items(population)
+            self.assertNotEqual(item1, item2,
+                                "it should always pick different items")
